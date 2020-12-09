df797e04302785bc5e05fe355e7f79dedd875310
Adam Bilchouris <thesteamyocelot@gmail.com>
2020-12-09 18:04:31 +1100

    not the best

--- Day 9/Main.cpp
+++ Day 9/Main.cpp
@@ -3,14 +3,16 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <regex>
 #include <utility>
-#include <set>
-#include <tuple>
+#include <numeric>
+#include <algorithm>
 
-std::vector<std::pair<std::string, int>> ReadInput();
+std::vector<long> ReadInput();
 void Part1();
 void Part2();
+long FindTroubledNumber(const std::vector<long> &vec, int offset);
+long FindMin(const std::vector<long> &vec, int low, int high);
+long FindMax(const std::vector<long> &vec, int low, int high);
 
 int main()
 {
@@ -19,11 +21,11 @@ int main()
     return 0;
 }
 
-std::vector<std::pair<std::string, int>> ReadInput()
+std::vector<long> ReadInput()
 {
     std::ifstream file("data.txt");
     std::string toRead;
-    std::vector<std::pair<std::string, int>> values;
+    std::vector<long> ret;
 
     while(std::getline(file, toRead))
     {
@@ -34,20 +36,124 @@ std::vector<std::pair<std::string, int>> ReadInput()
 
         else
         {
-            std::string instruction = toRead.substr(0, 3);
-            std::string valueStr = toRead.substr(4, toRead.length() - 1);
-            int value = std::stoi(valueStr);
-            values.push_back(std::make_pair(instruction, value));
+            long val = std::stol(toRead);
+            ret.push_back(val);
         }
     }
-    return values;
+    return ret;
 }
 
 void Part1()
 {
+    long ret = FindTroubledNumber(ReadInput(), 25);
+    std::cout << "Part 1 = " << ret << std::endl;
 }
 
 
 void Part2()
 {
+    long ret = FindTroubledNumber(ReadInput(), 25);
+    std::vector<long> initial = ReadInput();
+
+    long answer = -1;
+    long sum = 0;
+    int i, j = 0;
+    bool quit = false;
+
+    for(i = 0; i < initial.size() && !quit; i++)
+    {
+        j = i;
+        sum = 0;
+
+        while(sum < ret && !quit)
+        {
+            sum += initial[j];
+            
+            if((sum == ret) && ((j - i) > 1))
+            {
+                quit = true;
+            }
+
+            j++;
+        }
+    }
+
+    long min = FindMin(ReadInput(), i, j);
+    long max = FindMax(ReadInput(), i, j);
+
+    answer = min + max;
+    std::cout << "Part 2 = " << answer << std::endl;
+}
+
+long FindTroubledNumber(const std::vector<long> &vec, int offset)
+{
+    int startIndex = offset;
+    std::vector<long> initial = ReadInput();
+    long val = 0;
+    bool found = false;
+    std::vector<long> foundVals;
+
+    for(int i = startIndex; i < initial.size(); i++)
+    {
+        val = initial[i];
+        found = false;
+        for(int j = i - 1; j >= i - startIndex; j--)
+        {
+            for(int k = i - 2; k >= i - startIndex - 1; k--)
+            {
+                if(found)
+                {
+                    break;
+                }
+
+                if(initial[j] + initial[k] == val)
+                {
+                    foundVals.push_back(val);
+                    found = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    long problemValue = -1;
+    for(int i = 0; i < foundVals.size(); i++)
+    {
+        if(initial[i + startIndex] != foundVals[i])
+        {
+            problemValue = initial[i + startIndex];
+            break;
+        }
+    }
+
+    return problemValue;
+}
+
+
+long FindMin(const std::vector<long> &vec, int low, int high)
+{
+    long min = vec[low];
+    for(int k = low; k <= high; k++)
+    {
+        if(min > vec[k])
+        {
+            min = vec[k];
+        }
+    }
+
+    return min;
+}
+
+long FindMax(const std::vector<long> &vec, int low, int high)
+{
+    long max = 0;
+    for(int k = low; k <= high; k++)
+    {
+        if(max < vec[k])
+        {
+            max = vec[k];
+        }
+    }
+
+    return max;
 }
