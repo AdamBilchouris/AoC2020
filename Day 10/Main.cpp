20c2fd2c131c5e70abe37985bb8893e5c01ac64b
Adam Bilchouris <thesteamyocelot@gmail.com>
2020-12-11 13:55:30 +1100

    Forgot 0 and last for Part 1

--- Day 10/Main.cpp
+++ Day 10/Main.cpp
@@ -31,11 +31,14 @@ std::vector<int> ReadInput()
 
 void Part1()
 {
-    std::vector<int> values = ReadInput();
-    std::vector<int> difference;
+    std::vector<int> values;
+    values.push_back(0);
+    std::vector<int> tempValues = ReadInput();
+    values.insert(values.end(), tempValues.begin(), tempValues.end());
     std::sort(values.begin(), values.end());
+    values.push_back(values[values.size() - 1] + 3);
 
-    int outlet = 0;
+    std::vector<int> difference;
     int oneDiff = 0;
     int threeDiff = 0;
 
@@ -44,8 +47,8 @@ void Part1()
         difference.push_back(values[i + 1] - values[i]); 
     }
 
-    oneDiff = std::count(difference.begin(), difference.end(), 1) + 1;
-    threeDiff = std::count(difference.begin(), difference.end(), 3) + 1;
+    oneDiff = std::count(difference.begin(), difference.end(), 1);
+    threeDiff = std::count(difference.begin(), difference.end(), 3);
 
     std::cout << "Part 1: Product = " << oneDiff * threeDiff << std::endl;
 }
@@ -54,25 +57,25 @@ void Part2()
 {
     std::vector<int> values;
     values.push_back(0);
-
     std::vector<int> tempValues = ReadInput();
     values.insert(values.end(), tempValues.begin(), tempValues.end());
-
     std::sort(values.begin(), values.end());
-
     values.push_back(values[values.size() - 1] + 3);
+
     std::vector<long> combinations (values.size());
+    //Ways to get to 0 is 1.
     combinations[0] = 1;
 
     for(Index i = 0; i < values.size() - 1; i++)
     {
         //Check for out of range and if it is <= 3 jolts away from the current one
         //If both are true, then make the total number of ways to get to here
-        //number of ways beforehand and any current value.
+        //number of ways beforehand and any current value (which will just be the current value).
         //(0), 1, 4, 7, 10, 12, 15, 16, 19, (22) 
         //(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
         //If we are at 10, we can go to 11 OR 12, OR both.
         //This means that both are valid paths, so we give both the number of combinations currently.
+        //(i + 1) and (i + 2)
         //======================================================================
         //Make the index check up to +3, since every value is unique and in a sorted array, it
         //can only be at most 3 away if it is increasing by the whole time...
